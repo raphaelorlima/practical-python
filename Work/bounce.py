df10c70b8bbb4d6f760eda9151878af44fde0c0e
raphaelorlima <raphaelrequigo@gmail.com>
2025-07-26 12:43:47 -0300

    First changes in file work.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height: int = 100 # meters
+
+bounce_number: int = 0
+while bounce_number <= 10:
+    bounce_number += 1
+    height: float = 0.6 * float(height)
+    print(f"Bounce number: {bounce_number}; height: {round(height,2)}")
